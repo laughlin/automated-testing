ff8959fe919f444b501a61c252f1fa87bae63d15
Rachael Ferguson <rferguson@laughlin.com>
2018-03-23 13:27:48 -0500

    Try abstracting methods into a module

---
+++ automationtesting.py
@@ -0,0 +1,99 @@
+from bs4 import BeautifulSoup
+import urllib.request
+
+def check_matching(expected, actual):
+    if actual == expected:
+        match_result = "OK"
+    else:
+        match_result = "Expected and actual do not match"
+    return match_result
+
+#--- Parsing the url output to get a tag or link ---#
+def return_tag(url, tag):
+    ''' This function parses the html of the parameter url and returns the url of a canonical if set.'''
+    f = urllib.request.urlopen(url)
+    soup = BeautifulSoup(f.read(), 'html.parser')
+    
+    # Find all html elements that are of type link. Then, for each of these elements, if it is a canonical
+    # link, set this to be the canonical and break out of the loop. There can only be one canonical set so no need
+    # to check the rest of the links.
+    result = "None set"
+    if tag == "title":
+        for link in soup.find(tag):
+            result = link
+    else:
+        for link in soup.findAll('meta'):
+            if link.get('name') == tag:
+                result = link.get('content')
+    return result
+
+def return_canonical(url):
+    ''' This function parses the html of the parameter url and returns the url of a canonical if set.'''
+    f = urllib.request.urlopen(url)
+    soup = BeautifulSoup(f.read(), 'html.parser')
+
+    # Originally set canonical to be none. Need to override this if it does exist.
+    canonical = NO_CANONICAL_SET_STRING
+    
+    # Find all html elements that are of type link. Then, for each of these elements, if it is a canonical
+    # link, set this to be the canonical and break out of the loop. There can only be one canonical set so no need
+    # to check the rest of the links.
+    for link in soup.findAll('link'):
+        if link.get("rel")[0] == "canonical":
+            canonical = link.get('href')
+            break
+    return canonical
+    
+
+
+#--- Checking SEO ---#
+def check_seo_length(tag, min_length, max_length):
+    if len(tag) >= min_length and len(tag) <= max_length:
+        seo_result = "OK"
+    elif len(tag) < min_length:
+        seo_result = "Too short, should be >= "+str(min_length)
+    else:
+        seo_result = "Too long, should be <= "+str(max_length)
+    return seo_result
+
+def check_seo_hops(hops):
+    "Response is based on seo accepted number of hops"
+    if hops >= 3:
+        seo_check = "Correct redirect, but hopped three times or more."
+    else:
+        seo_check = "OK"
+    return seo_check
+
+
+#--- Involves adjusting urls for environment or parsing---#
+def change_env(url, is_ml_review, base):
+    url = return_full_clean_path(url, base)
+    if is_ml_review:
+        url = change_to_review(url) 
+    else:
+        url = change_to_prod(url)
+    return url
+
+def change_to_prod(url):
+    if 'review.masterlock' in url or 'review.sentrysafe' in url:
+        split_url = url.split('review')
+        url = "".join([split_url[0],"www", split_url[1]])
+    return url
+
+def change_to_review(url):
+    basic_path = url.split('www', 1)
+    review_url = "".join([basic_path[0],'review', basic_path[1]])
+    return review_url
+
+def get_base(ssl):
+    if ssl: base = SSL    
+    else: base = NON_SSL
+    return base
+
+def return_full_clean_path(url, ssl):
+    '''Adds the full url path if none was defined on the input file.'''
+    base = get_base(ssl)
+    url = url.strip().rstrip('/')
+    if "//" not in url:
+        url = "".join([base,"://",url])
+    return url
